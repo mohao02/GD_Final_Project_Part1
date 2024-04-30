51f4867fc90b0129c0bbd72f1f9b77a2a4a681f4
mamasa <mamasa@assetsense.com>
2024-04-30 14:16:33 -0400

    final project game

---
+++ Assets/Scripts/DetectCollisons.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetechCollisons : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other){
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
